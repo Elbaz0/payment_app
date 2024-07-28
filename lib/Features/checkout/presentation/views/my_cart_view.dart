9c6c3e92141572828a8e5952903dccca1e1e257c
ibrahimElbaz <ibrahim.elbaz.1011@gmail.com>
2024-07-28 07:44:49 +0300

    create style file

---
+++ lib/Features/checkout/presentation/views/my_cart_view.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class myCart_view extends StatelessWidget {
+  const myCart_view({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My Cart'),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:payment_app/Features/checkout/presentation/views/my_cart_view.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,11 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Payment'),
-        ),
-      ),
+      home: myCart_view(),
     );
   }
 }
